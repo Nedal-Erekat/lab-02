65c2fd4617cbefd649c615e22dc5ed711739b9d1
Nedal_Erekat <nedalerekat@gmail.com>
2020-06-30 18:42:29 +0300

    flex

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 'use strict'
 let gArr = [];
 let listOption = [];
+let templateId = '#templateToPhoto';
+let btn1Status = true;
+let btn2Status = false;
 function ProdautShow(title, imgUrl, decs, keyword, horns) {
     this.title = title;
     this.imgUrl = imgUrl;
@@ -9,27 +12,36 @@ function ProdautShow(title, imgUrl, decs, keyword, horns) {
     this.horns = horns;
     gArr.push(this);
 }
+
 ProdautShow.prototype.render = function () {
-    //    let templetCopy=$(".photo-template").clone();
 
-    //    .appendTo("main");
-    // let template = $('.photo-template').clone();
-    // console.log(template);
-    // templetCopy.removeClass('photo-template');
-    // template.text(this.title);
+    // solution1
+    /* let templetCopy = $(".photo-template").clone();
+     templetCopy.removeClass("photo-template")
+     templetCopy.find('h2').text(this.title);
+     templetCopy.find('img').attr('src', this.imgUrl);
+     templetCopy.find('p').text(this.decs);
+     $('.cont').append(templetCopy);*/
 
-    // console.log('hhhhhhhh');
-    // $(`${template} h2`).test(this.title);
-    // $('h2').text(this.title);
-    // $('img').attr(`src`, this.imgUrl);
-    // $('p').text(this.decs);
-    // $('main').append(templetCopy);
-    let div = $("<div></div>");
+
+
+
+    // solution 2
+    /*let div = $("<div></div>");
     let h2 = $("<h2></h2>").text(this.title);
     let imgEl = $("<img>").attr(`src`, this.imgUrl);
     let pEl = $("<p></p>").text(this.decs);
     $(".photo-template").append(div);
-    div.append(h2, imgEl, pEl);
+    div.append(h2, imgEl, pEl);*/
+
+    //solution 3 : mustache
+
+    let musTemplate = $('#templateToPhoto').html();
+    console.log(this);
+
+    let newObj = Mustache.render(musTemplate, this);
+    console.log(newObj);
+    $('.photo-template').append(newObj);
 };
 ProdautShow.prototype.renderList = function () {
     if (listOption.includes(this.keyword) === false) {
@@ -42,12 +54,41 @@ ProdautShow.prototype.renderList = function () {
 const ajaxSetting = { method: 'get', dataType: 'json' }
 $.ajax('./data/page-1.json', ajaxSetting)
     .then((data) => {
+        console.log("data: " + data);
+
         data.forEach(element => {
             let prod = new ProdautShow(element.title, element.image_url, element.description, element.keyword, element.horns);
             prod.render();
             prod.renderList();
         });
     });
+$('#first').on("click", () => {
+
+    location.reload();
+});
+$('#second').on("click", () => {
+    btn1Status = false;
+    btn2Status = true;
+    $('.photo-template').empty();
+    $.ajax('./data/page-2.json', ajaxSetting)
+        .then((data) => {
+            console.log("data: " + data);
+            gArr = [];
+            listOption = [];
+            $('option:not(:first-child)').css('display','none');
+            //    while ($('select:nth-child(2)')) {
+
+            //        $('select:nth-child(2)').remove();
+            //    }
+            // $('select').empty();
+            data.forEach(element => {
+                let prod = new ProdautShow(element.title, element.image_url, element.description, element.keyword, element.horns);
+                prod.render();
+                prod.renderList();
+            });
+        });
+})
+
 
 $('select').on("change", (evnt) => {
     $(".photo-template").empty();
@@ -57,25 +98,37 @@ $('select').on("change", (evnt) => {
     });
 
 
-    if(str==='Filter by Keyword'){
-        location.reload();
-        
+    if (str === 'Filter by Keyword') {
+        if (btn1Status) {//true//btn2 => false
+            location.reload();
+        } else if (btn2Status) {//true//btn1 => false
+            $('#second').trigger('click');
+        }
+        // location.reload();
+
     }
     gArr.forEach((item, i) => {
         let type = item.keyword;
-        console.log('type : '+type+' ,str: '+str);
-        console.log( 'result :'+ str ===type);
+        console.log('type : ' + type + ' ,str: ' + str);
+        console.log('result :' + str === type);
 
-        
-        if (str===type) {
-            console.log('reached hinsidde the condition');
-            
-            let div = $("<div></div>");
-            let h2 = $("<h2></h2>").text(item.title);
-            let imgEl = $("<img>").attr(`src`, item.imgUrl);
-            let pEl = $("<p></p>").text(item.decs);
-            $(".photo-template").append(div);
-            div.append(h2, imgEl, pEl);
+
+        if (str === type) {
+            // console.log('reached hinsidde the condition');
+
+            // let div = $("<div></div>");
+            // let h2 = $("<h2></h2>").text(item.title);
+            // let imgEl = $("<img>").attr(`src`, item.imgUrl);
+            // let pEl = $("<p></p>").text(item.decs);
+            // $(".photo-template").append(div);
+            // div.append(h2, imgEl, pEl);
+
+            let musTemplate = $('#templateToPhoto').html();
+            let newObj = Mustache.render(musTemplate, item);
+            $('.photo-template').append(newObj);
         }
     })
 })
+
+
+// add event to s
